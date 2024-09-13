0b635aba11172542d208cabbed907d79f3370f29
Nisha MR <nishassga@gmail.com>
2024-09-13 10:27:25 +0530

    first commit

---
+++ SubstringbegEndindexDemo.java
@@ -0,0 +1,9 @@
+package StringsDemo;
+
+public class SubstringbegEndindexDemo {
+
+    public static void main(String[] args) {
+        String str1="We are learning Java";
+        System.out.println("substring of a string="+str1.substring(0,10));
+    }
+}
